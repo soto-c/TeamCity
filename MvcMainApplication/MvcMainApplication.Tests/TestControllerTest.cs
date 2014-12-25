2cda80dc2dcf7d967a8ede2f4f94bb2a258cc265
soto-c <soto.c@smarto.co.th>
2014-12-25 14:02:22 +0700

    s

--- MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
+++ MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
@@ -68,7 +68,7 @@ namespace MvcMainApplication.Tests
         [TestMethod]
         public void IndexTest()
         {//ee
-            //Assert.Fail("Test failed!");
+            //SotoComment
 
             Assert.IsTrue(1 == 1);
         }
