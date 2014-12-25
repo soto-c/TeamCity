27ae6541207b7d508240d7454a7166203a1d4709
soto-c <soto.c@smarto.co.th>
2014-12-25 17:25:38 +0700

    Add Dependency

---
+++ MvcMainApplication/DepA/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DepA
+{
+    public class Class1
+    {
+
+        public Class1() { 
+
+
+        }
+    }
+}
