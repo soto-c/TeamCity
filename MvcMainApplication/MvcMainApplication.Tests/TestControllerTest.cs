09784d5933ff1200b1eeb157ead31742a584af50
soto-c <soto.c@smarto.co.th>
2014-12-26 11:34:50 +0700

    rf

--- MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
+++ MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
@@ -74,7 +74,7 @@ namespace MvcMainApplication.Tests
             // oesdfuiiopsda
             // MyFeature1 implemented
             // MyFeature2 implemented...i forgot something!!!
-            
+            // aAdd shit
             Assert.IsTrue(1 == 1);
         }
 
