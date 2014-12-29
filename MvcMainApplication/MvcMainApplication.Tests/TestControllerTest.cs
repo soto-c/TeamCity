7606c0b6e36d0be9897b73bce0c075fadc819cc8
soto-c <soto.c@smarto.co.th>
2014-12-29 09:26:23 +0700

    sdfsarfwerweqrewqrweq

--- MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
+++ MvcMainApplication/MvcMainApplication.Tests/TestControllerTest.cs
@@ -68,7 +68,7 @@ namespace MvcMainApplication.Tests
         [TestMethod]
         public void IndexTest()
         {
-            //Test trigger filter +:Soto*
+            //Test trigger filter +:Soto* ddd
             // Add comment buid +:comment=[Build]:**
             //refs/heads/Soto  
               Assert.IsTrue(1 == 1);
